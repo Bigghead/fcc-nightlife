a1c80d695674ec4a0a5349edb28048ae3aa80a08
Shav <shavenchew@yahoo.com>
2016-12-15 14:52:37 -0800

    Figuring out yelp's api

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var express    = require('express'),
+var express      = require('express'),
       mongoose   = require('mongoose'),
       bodyParser = require('body-parser'),
+      cors       = require('cors'),
+      Yelp       = require('yelp'),
       app        = express();
 
 
@@ -8,13 +10,32 @@ var express    = require('express'),
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/nightlife');
 
+
 //Setup
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+  console.log(res);
+  next();
+});
+
 
 app.get('/', function(req, res){
   res.render('landing');
+});
+
+app.get('/home', function(req, res){
+  res.render('homepage');
+});
+
+app.post('/home', function(req, res){
+  var city = req.body.cityName;
+  console.log(req);
+  res.render('homepage', {city : city});
 });
 
 
