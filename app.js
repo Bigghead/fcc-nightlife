a300c3ab680ddff3b6b6c45c01a012585beb0ef4
Shav <shavenchew@yahoo.com>
2016-12-15 16:21:42 -0800

    Mostly working homepage

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express      = require('express'),
       bodyParser = require('body-parser'),
       cors       = require('cors'),
       Yelp       = require('yelp'),
+      passport   = require('passport'),
+      localStrategy = require('passport-local'),
+      passportLocalMongoose = require('passport-local-mongoose'),
+      Session    = require('express-session'),
       app        = express();
 
 
@@ -10,6 +14,13 @@ var express      = require('express'),
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/nightlife');
 
+//=====YELP====
+var yelp = new Yelp({
+  consumer_key: 'sfgPlJGSBNW5aBQIDC5i4Q',
+  consumer_secret: 'SqxCS3NS4Kc1odi3w27Er3poObQ',
+  token: 'AoHJqZ5z7z2s0py6SbABlazi62xV9Isg',
+  token_secret: 'o8U5q3AWEIf8MXTS-6O_uWPmgH8',
+});
 
 //Setup
 app.set('view engine', 'ejs');
@@ -19,7 +30,7 @@ app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
-  console.log(res);
+
   next();
 });
 
@@ -28,14 +39,20 @@ app.get('/', function(req, res){
   res.render('landing');
 });
 
-app.get('/home', function(req, res){
-  res.render('homepage');
-});
 
 app.post('/home', function(req, res){
-  var city = req.body.cityName;
-  console.log(req);
-  res.render('homepage', {city : city});
+  var cityName = req.body.cityName;
+  yelp.search({
+    term: 'food',
+    location: cityName
+  }, function(err, data){
+    if(err){
+      console.log(err);
+    } else {
+        res.render('homepage', {data : data});
+    }
+  });
+
 });
 
 
